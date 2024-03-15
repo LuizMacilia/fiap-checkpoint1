ba1e01678b64a369a5f359a95364f1db8b2135bc
LuizMacilia <luizmancilia11@gmail.com>
2024-03-14 23:09:30 -0300

    checkpoint1

---
+++ src/main/java/br/com/fiap/checkpoint1/Checkpoin1Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpoint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoin1Application.class, args);
+	}
+
+}
